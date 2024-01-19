230319ab43fee7dfea0c8dc417e4e3095e1deee8
jagratiagarwal <154362009+jagratiagarwal@users.noreply.github.com>
2024-01-20 01:05:59 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull jagratiagarwal/my-first-build-image:latest 
+docker pull jagratiagarwal/my-first-build-image:latest
 
 # Run the Docker image as a container
 docker run -d -p 8000:8000 jagratiagarwal/my-first-build-image:latest
