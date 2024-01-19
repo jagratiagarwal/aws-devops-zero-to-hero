60ddc3ade399f7fe156a7956a7adf62380a9577e
jagratiagarwal <154362009+jagratiagarwal@users.noreply.github.com>
2024-01-19 22:41:59 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,8 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull jagratiagarwal/my-first-build-image:latest  
-
+echo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 jagratiagarwal/my-first-build-image:latest
+echo
