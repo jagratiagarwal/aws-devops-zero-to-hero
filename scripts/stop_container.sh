a967ebf5f8ad3cd0f68b8d6f43f036ba4e69f440
jagratiagarwal <154362009+jagratiagarwal@users.noreply.github.com>
2024-01-21 19:58:49 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,8 +2,8 @@
 set -e 
 
 # Stop the running container (if any)
-container_id=docker ps | awk -F " " '{print $1}'
+container_id=$(docker ps | awk -F " " '{print $1}')
 #echo "$conatiner_id"
-docker conatiner rm -f $container_id
+docker rm -f $container_id
 
 
